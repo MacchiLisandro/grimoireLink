f8ae96ad5307aeacde586e90d9da21ff6bf13f4d
MacchiLisandro <134971163+MacchiLisandro@users.noreply.github.com>
2026-04-27 20:07:59 -0300

    Initial commit

---
+++ src/main/java/com/maliag/grimoireLink/GrimoireLinkApplication.java
@@ -0,0 +1,13 @@
+package com.maliag.grimoireLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimoireLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrimoireLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maliag/grimoireLink/GrimoireLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maliag.grimoireLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrimoireLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
