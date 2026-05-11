5bd3320996bffe83b96f8675100bb5273df9bc20
mbenegasp <mbenegas2001@gmail.com>
2026-05-11 20:30:08 -0300

    feature: Create MonsterEntity

--- src/main/java/com/maliag/grimoireLink/features/users/UserEntity.java
+++ src/main/java/com/maliag/grimoireLink/features/users/UserEntity.java
@@ -1,19 +1,14 @@
 package com.maliag.grimoireLink.features.users;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Builder
-@Component
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
