3fec6870387572482e049942ec8d55475b3e2caf
MacchiLisandro <134971163+MacchiLisandro@users.noreply.github.com>
2026-05-13 20:33:43 -0300

    feature/Create SpellsXCharacterEntity

--- src/main/java/com/maliag/grimoireLink/features/monsters/MonsterEntity.java
+++ src/main/java/com/maliag/grimoireLink/features/monsters/MonsterEntity.java
@@ -17,22 +17,31 @@ public class MonsterEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column (name = "public_id", unique = true, nullable = false, updatable = false)
     private UUID publicId;
+
     @Column (name = "name", nullable = false)
     private String name;
+
     @Column (name = "level", nullable = false)
     private Integer level;
+
     @Column (name = "max_hp", nullable = false)
     private Integer maxHp;
+
     @Column (name = "current_hp", nullable = false)
     private Integer currentHp;
+
     @Column (name = "strength", nullable = false)
     private Integer strength;
+
     @Column (name = "dexterity", nullable = false)
     private Integer dexterity;
+
     @Column (name = "constitution", nullable = false)
     private Integer constitution;
+
     @Column (name = "is_alive", nullable = false)
     private Boolean isAlive;
 
