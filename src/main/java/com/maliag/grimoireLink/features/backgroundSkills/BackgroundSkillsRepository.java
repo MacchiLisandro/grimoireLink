ae1ffbd1247cc0a6b537a4091731ae9fff3d8ebf
MacchiLisandro <134971163+MacchiLisandro@users.noreply.github.com>
2026-06-06 17:39:45 -0300

    Add BackgroundSkills and Repository
    Add BackgroundSkillsEntity
    Add BackgroundSkillRepository

---
+++ src/main/java/com/maliag/grimoireLink/features/backgroundSkills/BackgroundSkillsRepository.java
@@ -0,0 +1,13 @@
+package com.maliag.grimoireLink.features.backgroundSkills;
+
+import com.maliag.grimoireLink.features.background.BackgroundEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BackgroundSkillsRepository extends JpaRepository<BackGroundSkillsEntity,Long> {
+
+    List<BackGroundSkillsEntity>findByBackground(BackgroundEntity background);
+}
