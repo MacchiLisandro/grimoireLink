3eb24021aabf352e5141d311e308c93befc00049
Agostina <afenoyrivas@gmail.com>
2026-06-07 19:03:51 -0300

    fix CampaignRequest and UpdateCampaignRequest
    -Add @NoArgsConstructor and @AllArgsConstructor to CampaignRequest and UpdateCampaignRequest

--- src/main/java/com/maliag/grimoireLink/features/campaign/dto/CampaignRequest.java
+++ src/main/java/com/maliag/grimoireLink/features/campaign/dto/CampaignRequest.java
@@ -2,10 +2,14 @@ package com.maliag.grimoireLink.features.campaign.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CampaignRequest {
 
