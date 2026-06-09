fca7dbef7bc8050eeb14bb4bbd546934689c2ff1
mbenegasp <mbenegas2001@gmail.com>
2026-06-09 04:48:21 -0300

    encounter service finished

--- src/main/java/com/maliag/grimoireLink/features/encounter/EncounterMapper.java
+++ src/main/java/com/maliag/grimoireLink/features/encounter/EncounterMapper.java
@@ -1,11 +1,13 @@
 package com.maliag.grimoireLink.features.encounter;
 
 import com.maliag.grimoireLink.features.characters.CharacterSummaryMapper;
+import com.maliag.grimoireLink.features.encounter.dto.EncounterRequest;
 import com.maliag.grimoireLink.features.encounter.dto.EncounterResponse;
 import com.maliag.grimoireLink.features.monsters.MonsterMapper;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", uses = {CharacterSummaryMapper.class, MonsterMapper.class})
 public interface EncounterMapper {
-    EncounterResponse toEncounterResponse (EncounterEntity encounter);
+    EncounterResponse toResponse(EncounterEntity encounter);
+    EncounterEntity toEntity(EncounterRequest encounter);
 }
--- src/main/java/com/maliag/grimoireLink/features/encounter/EncounterService.java
+++ src/main/java/com/maliag/grimoireLink/features/encounter/EncounterService.java
@@ -1,4 +1,16 @@
 package com.maliag.grimoireLink.features.encounter;
 
+import com.maliag.grimoireLink.features.encounter.dto.EncounterRequest;
+import com.maliag.grimoireLink.features.encounter.dto.EncounterResponse;
+
+import java.util.UUID;
+
 public interface EncounterService {
+    EncounterResponse saveEncounter(EncounterRequest request);
+    EncounterResponse getEncounterById(UUID encounterId);
+    EncounterResponse updateStatusEncounter(UUID encounterId, EncounterStatus status);
+    EncounterResponse addCharacter(UUID encounterId, UUID characterId);
+    EncounterResponse addMonster(UUID encounterId, String monsterId);
+    EncounterResponse updateCharacterHp(UUID encounterId, UUID characterId, int newHp);
+    EncounterResponse updateMonsterHp(UUID encounterId, UUID monsterId, int newHp);
 }
