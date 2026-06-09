c8107dc935b4db1faf257d7919011620ea0578dd
mbenegasp <mbenegas2001@gmail.com>
2026-06-09 06:19:18 -0300

    Add @Mapping annotations in toEntity() in EncounterMapper

--- src/main/java/com/maliag/grimoireLink/features/encounter/EncounterMapper.java
+++ src/main/java/com/maliag/grimoireLink/features/encounter/EncounterMapper.java
@@ -5,9 +5,14 @@ import com.maliag.grimoireLink.features.encounter.dto.EncounterRequest;
 import com.maliag.grimoireLink.features.encounter.dto.EncounterResponse;
 import com.maliag.grimoireLink.features.monsters.MonsterMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {CharacterSummaryMapper.class, MonsterMapper.class})
 public interface EncounterMapper {
     EncounterResponse toResponse(EncounterEntity encounter);
+    @Mapping(target = "publicId", ignore = true)
+    @Mapping(target = "characters", ignore = true)
+    @Mapping(target = "monsters", ignore = true)
+    @Mapping(target = "encounterStatus", ignore = true)
     EncounterEntity toEntity(EncounterRequest encounter);
 }
