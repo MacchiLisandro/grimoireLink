be63377f7960ed8809b29ee5c5c42fd80e5c7a26
mbenegasp <mbenegas2001@gmail.com>
2026-06-10 08:28:04 -0300

    fix updateHp method in CharacterServiceImpl

--- src/main/java/com/maliag/grimoireLink/features/characters/CharacterServiceImpl.java
+++ src/main/java/com/maliag/grimoireLink/features/characters/CharacterServiceImpl.java
@@ -27,7 +27,6 @@ import com.maliag.grimoireLink.features.usersXCampaign.UsersXCampaignRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
