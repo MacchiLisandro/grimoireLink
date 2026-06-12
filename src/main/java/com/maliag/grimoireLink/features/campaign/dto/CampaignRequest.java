38f4bd685fb4e80ca86912ce1b8fb83fa4fd06be
Agostina <afenoyrivas@gmail.com>
2026-06-12 04:22:48 -0300

    Add campaign dtos validation messages

--- src/main/java/com/maliag/grimoireLink/features/campaign/dto/CampaignRequest.java
+++ src/main/java/com/maliag/grimoireLink/features/campaign/dto/CampaignRequest.java
@@ -13,10 +13,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CampaignRequest {
 
-    @NotBlank
+    @NotBlank(message = "Se debe incluir el nombre de la campaña")
     private String name;
 
-    @NotBlank
-    @Size(max = 65535)
+    @NotBlank(message = "Se debe incluir la descripción de la campaña")
+    @Size(max = 65535, message = "La descripción no puede tener más de 65535 caracteres")
     private String description;
 }
